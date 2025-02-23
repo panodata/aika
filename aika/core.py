7e76b63e89f72c4c90c0057973c9da7d6503e2fb
Andreas Motl <andreas.motl@panodata.org>
2025-02-24 00:48:57 +0100

    Add new "dudp" parser, using `python-dateutil` and `dateparser`

--- aika/core.py
+++ aika/core.py
@@ -1,24 +1,33 @@
-# Copyright (c) 2023, The Panodata developers and contributors.
+# Copyright (c) 2023-2025, The Panodata developers and contributors.
 # Distributed under the terms of the LGPL license, see LICENSE.
 
 import datetime as dt
 import logging
 import typing as t
+import warnings
 
-from arbitrary_dateparser import DateParser
+import dateparser
+import dateutil.parser.isoparser
+import fiscalyear
 from daterangeparser import parse as drp_parse_english
-from pendulum import Period
+from dateutil.rrule import MONTHLY, WEEKLY
 
-from .dateparser_german import DateParserGerman
 from .daterangeparser_german import parse_german as drp_parse_german
-from .model import Parser, trange
+from .model import Parser, TimeInterval, trange
+
+try:
+    from arbitrary_dateparser import DateParser
+except ImportError:
+    warnings.warn(
+        "arbitrary-dateparser lacks support for recent versions of Python", category=ImportWarning, stacklevel=2
+    )
+
+if t.TYPE_CHECKING:
+    import pandulum
+
 
 logger = logging.getLogger(__name__)
 
-# FIXME: Do not set timezone explicitly.
-arbitrary_parser_english = DateParser(tz="Europe/Berlin")
-arbitrary_parser_english.replaced_words["in"] = "this"
-arbitrary_parser_german = DateParserGerman(tz="Europe/Berlin")
 
 before_midnight = dt.time(hour=23, minute=59, second=59, microsecond=999999)
 midnights = [dt.time(hour=0, minute=0, second=0), before_midnight]
@@ -34,9 +43,17 @@ class DaterangeExpression:
       Examples: next week, tomorrow to next thursday.
     """
 
-    def __init__(self, default_start_time: t.Optional[dt.time] = None, default_end_time: t.Optional[dt.time] = None):
+    def __init__(
+        self,
+        default_start_time: t.Optional[dt.time] = None,
+        default_end_time: t.Optional[dt.time] = None,
+        midnight_heuristics: bool = False,
+        return_tuple: bool = False,
+    ):
         self.default_start_time = default_start_time
         self.default_end_time = default_end_time
+        self.return_tuple = return_tuple
+        self.midnight_heuristics = midnight_heuristics
         self.parsers: t.List[Parser] = []
         self.use_all_parsers()
 
@@ -46,6 +63,7 @@ class DaterangeExpression:
             Parser(name="DateRangeParser [de]", fun=drp_parse_german),
             Parser(name="arbitrary-dateparser [de]", fun=adp_parse_german),
             Parser(name="arbitrary-dateparser [en]", fun=adp_parse_english),
+            Parser(name="DUDP [all]", fun=dudp_parse),
         ]
 
     def clear_parsers(self):
@@ -54,7 +72,7 @@ class DaterangeExpression:
     def add_parser(self, fun: t.Callable, name: str = "unknown"):
         self.parsers.append(Parser(name=name, fun=fun))
 
-    def parse(self, when: str) -> trange:
+    def parse(self, when: str) -> t.Union[trange, TimeInterval]:
         """
         Parse date range from textual expression.
         """
@@ -68,35 +86,48 @@ class DaterangeExpression:
             try:
                 date_start, date_end = parser.fun(when)
                 break
-            except Exception:
-                logger.warning(f"Parsing date range failed ({parser.name}): {when}")
+            except Exception as ex:
+                logger.debug(f"Parsing date range failed ({parser.name}) for '{when}': {ex}")
 
         if date_start is None:
-            raise ValueError(f"Failed parsing date range: {when}")
+            raise ValueError(f"Failed detecting start date: {when}")
 
-        if date_start.time() in midnights and self.default_start_time is not None:
-            date_start = dt.datetime.combine(date_start, self.default_start_time)
+        if self.midnight_heuristics:
+            if date_start.time() in midnights and self.default_start_time is not None:
+                date_start = dt.datetime.combine(date_start, self.default_start_time)
 
-        if date_end is not None and date_end.time() in midnights:
-            if self.default_end_time is not None:
-                time = self.default_end_time
-            else:
-                time = dt.time(hour=23, minute=59, second=59, microsecond=999999)
-            date_end = dt.datetime.combine(date_end, time)
+            if date_end is not None and date_end.time() in midnights:
+                if self.default_end_time is not None:
+                    time = self.default_end_time
+                else:
+                    time = dt.time(hour=23, minute=59, second=59, microsecond=999999)
+                date_end = dt.datetime.combine(date_end, time)
 
-        return date_start, date_end
+        if self.return_tuple:
+            return date_start, date_end
+
+        return TimeInterval(date_start, date_end)
 
     def parse_single(self, when: str) -> dt.datetime:
         """
         Parse single date from textual expression.
         """
-        return self.parse(when)[0]
+        ti = self.parse(when)
+        if isinstance(ti, TimeInterval):
+            return ti.start
+        elif isinstance(ti, tuple):
+            return ti[0]
+        else:
+            raise TypeError(f"Invalid time interval type: {type(ti)}")
 
 
 def adp_parse_english(when: str) -> trange:
     """
     Parse date range using `arbitrary-dateparser`. English variant.
     """
+    # FIXME: Do not set timezone explicitly.
+    arbitrary_parser_english = DateParser(tz="Europe/Berlin")
+    arbitrary_parser_english.replaced_words["in"] = "this"
     return from_pendulum(arbitrary_parser_english(when))
 
 
@@ -104,10 +135,14 @@ def adp_parse_german(when: str) -> trange:
     """
     Parse date range using `arbitrary-dateparser`. German variant.
     """
+    from .dateparser_german import DateParserGerman
+
+    # FIXME: Do not set timezone explicitly.
+    arbitrary_parser_german = DateParserGerman(tz="Europe/Berlin")
     return from_pendulum(arbitrary_parser_german(when))
 
 
-def from_pendulum(period: Period) -> trange:
+def from_pendulum(period: "pandulum.Period") -> trange:
     """
     Translate Pendulum `Period` to tuple of `datetime` objects.
     """
@@ -118,3 +153,45 @@ def from_pendulum(period: Period) -> trange:
     date_start = period.start.naive()
     date_end = period.end.naive()
     return pendulum_to_datetime(date_start), pendulum_to_datetime(date_end)
+
+
+def dudp_parse(when: str) -> trange:
+    """
+    Parse date range using `python-dateutil` and `dateparser` libraries.
+    """
+
+    if ".." in when:
+        return t.cast(trange, when.split(".."))
+
+    if "Q" in when:
+        year, quarter = when.split("Q")
+        q = fiscalyear.FiscalQuarter(int(year), int(quarter)).next_fiscal_quarter
+        return q.start, q.end
+
+    interval = None
+    if "M" in when or (when.count("-") == 1):
+        when = when.replace("M", "-")
+        interval = MONTHLY
+    elif "W" in when:
+        interval = WEEKLY
+
+    t_start: t.Optional[dt.datetime] = None
+    try:
+        if "-" in when or "W" in when:
+            t_start = dateutil.parser.isoparse(when)
+        else:
+            t_start = dateutil.parser.parse(when)
+    except dateutil.parser.ParserError:
+        t_start = dateparser.parse(when)
+
+    if t_start is None:
+        raise ValueError(f"Failed parsing date range: {when}")
+
+    if interval == MONTHLY:
+        t_end = t_start + dt.timedelta(days=31)
+    elif interval == WEEKLY:
+        t_end = t_start + dt.timedelta(weeks=1)
+    else:
+        t_end = dt.datetime.today().replace(hour=23, minute=59, second=59, microsecond=999999)
+
+    return t_start, t_end
