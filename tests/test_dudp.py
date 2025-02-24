69b13d0103843dfc846cbd85fdd8a43d397cfa32
Andreas Motl <andreas.motl@panodata.org>
2025-02-24 07:23:13 +0100

    Tests: Improve test cases for relative human time intervals
    Like {last,this,next} {week,month,year}.

--- tests/test_dudp.py
+++ tests/test_dudp.py
@@ -1,9 +1,13 @@
 import datetime as dt
 
+import pytest
+from freezegun import freeze_time
+
 from aika import TimeInterval
+from tests.conftest import TESTDRIVE_DATETIME
 
 
-def test_calendar_week(ti):
+def test_week_calendar(ti):
     """
     Test calendar weeks.
     """
@@ -13,9 +17,45 @@ def test_calendar_week(ti):
     )
 
 
-def test_month(ti):
+@freeze_time(TESTDRIVE_DATETIME)
+def test_week_last(ti):
     """
-    Test months.
+    Test relative weeks.
+    """
+    # This is 2023W32.
+    assert ti.parse("last week") == TimeInterval(
+        dt.datetime(2023, 8, 7, 0, 0),
+        dt.datetime(2023, 8, 13, 23, 59, 59, 999999),
+    )
+
+
+@freeze_time(TESTDRIVE_DATETIME)
+def test_week_this(ti):
+    """
+    Test relative weeks.
+    """
+    # This is 2023W33.
+    assert ti.parse("this week") == TimeInterval(
+        dt.datetime(2023, 8, 14, 0, 0),
+        dt.datetime(2023, 8, 20, 23, 59, 59, 999999),
+    )
+
+
+@freeze_time(TESTDRIVE_DATETIME)
+def test_week_next(ti):
+    """
+    Test relative weeks.
+    """
+    # This is 2023W33.
+    assert ti.parse("next week") == TimeInterval(
+        dt.datetime(2023, 8, 21, 0, 0),
+        dt.datetime(2023, 8, 27, 23, 59, 59, 999999),
+    )
+
+
+def test_month_calendar(ti):
+    """
+    Test calendar months.
     """
     assert (
         ti.parse("2025M01")
@@ -24,6 +64,42 @@ def test_month(ti):
             dt.datetime(2025, 1, 1, 0, 0),
             dt.datetime(2025, 2, 1, 0, 0),
         )
+    )
+
+
+@freeze_time(TESTDRIVE_DATETIME)
+def test_month_last(ti):
+    """
+    Test relative months.
+    """
+    # This is 2023-07.
+    assert ti.parse("last month") == TimeInterval(
+        dt.datetime(2023, 7, 1, 0, 0),
+        dt.datetime(2023, 7, 31, 23, 59, 59, 999999),
+    )
+
+
+@freeze_time(TESTDRIVE_DATETIME)
+def test_month_this(ti):
+    """
+    Test relative months.
+    """
+    # This is 2023-08.
+    assert ti.parse("this month") == TimeInterval(
+        dt.datetime(2023, 8, 1, 0, 0),
+        dt.datetime(2023, 8, 31, 23, 59, 59, 999999),
+    )
+
+
+@freeze_time(TESTDRIVE_DATETIME)
+def test_month_next(ti):
+    """
+    Test relative months.
+    """
+    # This is 2023-09.
+    assert ti.parse("next month") == TimeInterval(
+        dt.datetime(2023, 9, 1, 0, 0),
+        dt.datetime(2023, 9, 30, 23, 59, 59, 999999),
     )
 
 
@@ -37,11 +113,49 @@ def test_quarter(ti):
     )
 
 
-def test_year(ti):
+def test_year_calendar(ti):
     """
-    Test years.
+    Test calendar years.
     """
     assert ti.parse("2025") == TimeInterval(
         dt.datetime(2025, 1, 1, 0, 0),
         dt.datetime(2026, 1, 1, 0, 0, 0),
+    )
+
+
+@freeze_time(TESTDRIVE_DATETIME)
+@pytest.mark.xfail()
+def test_year_last(ti):
+    """
+    Test relative years.
+    """
+    # This is 2022.
+    assert ti.parse("last year") == TimeInterval(
+        dt.datetime(2022, 1, 1, 0, 0),
+        dt.datetime(2022, 12, 31, 23, 59, 59, 999999),
+    )
+
+
+@freeze_time(TESTDRIVE_DATETIME)
+def test_year_this(ti):
+    """
+    Test relative years.
+    """
+    # This is 2023.
+    assert ti.parse("this year") == TimeInterval(
+        dt.datetime(2023, 1, 1, 0, 0),
+        dt.datetime(2023, 12, 31, 23, 59, 59, 999999),
+    )
+
+
+@freeze_time(TESTDRIVE_DATETIME)
+@pytest.mark.xfail()
+def test_year_next(ti):
+    """
+    Test relative years.
+    """
+    # This is 2024.
+    assert ti.parse("next year") == TimeInterval(
+        dt.datetime(2024, 1, 1, 0, 0),
+        dt.datetime(2024, 12, 31, 23, 59, 59, 999999),
     )
